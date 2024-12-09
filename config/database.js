4d5bdd5626d375795e4ef408dc8fa269056c0205
beola134 <164470971+beola134@users.noreply.github.com>
2024-12-09 16:25:17 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('duantotnghiep', 'root', '', {
-  host: 'localhost',
+  host: '127.0.0.1',
   dialect: 'mysql',
-  logging: false
+  port: 3306,
 
 });
 
