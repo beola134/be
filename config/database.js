0083a430b8f29129f2d9d9cbdf5540e243e7dd7f
beola134 <nguyentantai612004@gmail.com>
2024-12-10 13:43:44 +0700

    fix

--- config/database.js
+++ config/database.js
@@ -1,10 +1,21 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('duantotnghiep', 'root', '', {
-  host: 'localhost',
+// Thay thế các thông tin sau bằng thông tin từ Clever Cloud
+const sequelize = new Sequelize('DATABASE_NAME', 'USER', 'PASSWORD', {
+  host: 'HOST',
   dialect: 'mysql',
-  logging: false
-
+  port: 3306, // Cổng mặc định cho MySQL
+  logging: false, // Tắt logging nếu không cần
+  dialectOptions: {
+    ssl: {
+      require: true, // Clever Cloud yêu cầu SSL
+      rejectUnauthorized: false, // Không kiểm tra chứng chỉ
+    },
+  },
 });
+
+sequelize.authenticate()
+  .then(() => console.log('Kết nối cơ sở dữ liệu thành công!'))
+  .catch((error) => console.error('Lỗi kết nối:', error));
 
 module.exports = sequelize;
