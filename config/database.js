61a06cdc6f02808e07bb111c38e212b75d0ef1b0
beola134 <nguyentantai612004@gmail.com>
2024-12-10 12:09:12 +0700

    fix

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('duantotnghiep', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-
-});
+// Sử dụng biến môi trường
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE, // Tên cơ sở dữ liệu
+  process.env.DB_USER,     // Tên người dùng
+  process.env.DB_PASSWORD, // Mật khẩu
+  {
+    host: process.env.DB_HOST,  // Địa chỉ host
+    dialect: process.env.DB_DIALECT || 'mysql', // Loại CSDL
+    logging: false              // Vô hiệu hóa log (tùy chọn)
+  }
+);
 
 module.exports = sequelize;
